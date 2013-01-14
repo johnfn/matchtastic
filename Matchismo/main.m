f5a1827b6641b8abbf119647453202ff836e3aed
Grant Mathews <johnfn@gmail.com>
2013-01-14 09:54:44 -0800

    Initial Commit

---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Grant Mathews on 1/14/13.
+//  Copyright (c) 2013 johnfn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
