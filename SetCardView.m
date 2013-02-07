3a4c54afc157deb74f4591e24cf745025f30134c
Grant Mathews <johnfn@gmail.com>
2013-02-06 22:53:00 -0800

    Accomodate for multiple symbols.

--- SetCardView.m
+++ SetCardView.m
@@ -48,10 +48,44 @@
         return;
     }
     
-    NSLog(@"Good, we have color and calling drawRect");
+    int count = [self.count integerValue];
+    int symbolHeight = rect.size.height / 6;
     
-    [[UIColor orangeColor] setFill];
-    [[UIBezierPath bezierPathWithOvalInRect:rect] fill];
+    [self.color setFill];
+    //[[UIBezierPath bezierPathWithOvalInRect:rect] fill];
+    
+    [self.color setFill];
+    
+    for (int i = 0; i < count; i++) {
+        // This line of code calculates the y position of the symbol, no matter the number of symbols.
+        int itemY = rect.size.height/2 - symbolHeight * (count - 1) + i * symbolHeight * 2;
+        
+        [self drawSymbol:self.symbol inRect:rect atX:rect.size.width/2 atY:itemY];
+    }
+}
+
+- (void)drawSymbol:(NSString *)symbol inRect:(CGRect)rect atX:(int)x atY:(int)y {
+    int offset = rect.size.width / 10;
+    int symbolWidth = rect.size.width - offset * 2;
+    int symbolHeight = rect.size.height / 6;
+    
+    // Diamond
+    if (symbol == @"▲") {
+        NSLog(@"!");
+        CGRect dest = CGRectMake(x - symbolWidth / 2, y - symbolHeight / 2, symbolWidth, symbolHeight);
+        NSLog(@"Dest: %f %f %f %f", dest.origin.x, dest.origin.y, dest.size.width, dest.size.height);
+        [[UIBezierPath bezierPathWithOvalInRect:dest] fill];
+    }
+    
+    // Oval
+    if (symbol == @"●") {
+        
+    }
+    
+    // Squiggly
+    if (symbol == @"■") {
+        
+    }
 }
 
 @end
